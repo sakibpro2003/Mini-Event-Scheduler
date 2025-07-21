60fd0065dfade289ef00ccafcf3f5c9adf1b5c46
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-07-21 13:00:35 +0600

    feat: index.ts for types, created

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [react()],
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
